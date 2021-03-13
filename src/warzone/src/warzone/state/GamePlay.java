86043d43c3bff8d1a7f5543399461c25950df7c6
fzuray <ray.linqh@gmail.com>
2021-03-13 00:19:43 -0500

    update state refactoring

---
+++ src/warzone/src/warzone/state/GamePlay.java
@@ -0,0 +1,51 @@
+package warzone.state;
+import warzone.service.*;
+import warzone.model.*;
+import warzone.view.*;
+
+/**
+ *	ConcreteState of the State pattern. In this example, defines behavior 
+ *  for commands that are valid in this state, and for the others signifies  
+ *  that the command is invalid. 
+ */
+public class GamePlay extends Phase {
+
+	public GamePlay(GameEngine p_ge) {
+		super(p_ge);
+	}
+
+	 public void showMap() {
+		 //todo
+	 }	
+	
+	 public void addContinent(String p_parameters){
+		 printInvalidCommandMessage();
+	 }	
+	 public void removeContinent(String p_parameters) {
+		 printInvalidCommandMessage();
+	 }	
+	 public void addCountry (String p_parameters) {
+		 printInvalidCommandMessage();
+	 }	
+	 public void removeCountry(String p_parameters) {
+		 printInvalidCommandMessage();
+	 }	
+	 public void addNeighbor (String p_parameters) {
+		 printInvalidCommandMessage();
+	 }		
+	 public void removeNeighbor (String p_parameters) {
+		 printInvalidCommandMessage();
+	 }		 
+	
+	 public void saveMap (String p_fileName) {
+		 printInvalidCommandMessage();
+	 }	
+	 public void editMap (String p_fileName) {
+		 printInvalidCommandMessage();
+	 }		
+	 public void validateMap() {
+		 printInvalidCommandMessage();
+	 }		
+	 
+
+}
