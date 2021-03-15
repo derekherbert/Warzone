4debeeb64ba850b92a5ff35046247ebcd75bf52e
amyhcy <amy.chuanyanhu@gmail.com>
2021-03-16 04:49:24 +0800

    refactor map editor to state pattern

--- src/warzone/src/warzone/state/MapEditor.java
+++ src/warzone/src/warzone/state/MapEditor.java
@@ -8,6 +8,12 @@ import warzone.controller.MapController;
 import warzone.model.*;
 import warzone.view.*;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *	ConcreteState of the State pattern. In this example, defines behavior 
  *  for commands that are valid in this state, and for the others signifies  
@@ -19,7 +25,11 @@ public class MapEditor extends Phase {
 	private ContinentService d_continentService;
 	private CountryService d_countryService;
 	private NeighborService d_neighborService;
-	
+
+	/**
+	 * Constructor for MapEditor
+	 * @param p_ge Game Engine
+	 */
 	public MapEditor(GameEngine p_ge) {
 		super(p_ge);
 		d_mapService = new MapService(d_gameContext);
@@ -296,181 +306,81 @@ public class MapEditor extends Phase {
 	 *
 	 * Displays the map as text, showing all continents and countries and their respective neighbors.
 	 */
-	public void showMap () {
-
+	public void showMap() {
 		MapView.printMap(d_gameContext);
 	}
-	
-	 public void saveMap (String p_fileName) {
-		 //todo
-	 }	
-	
+
 	/**
-	 * Load a map from an existing â€œdominationâ€� map file,
-	 * or create a new map from scratch if the file does not exist.
-	 * 
-	 * @param p_fileName file name
-	 */	
-	public void editMap (String p_fileName) {
-		
-		String l_mapDirectory = WarzoneProperties.getWarzoneProperties().getGameMapDirectory();
-			
-		try {
-			
-			//Clear gameContext
-			d_gameContext.reset();
+	 * Performs the action for the user command: savemap filename
+	 *
+	 * Save a map to a text file exactly as edited (using the "domination" game map format).
+	 * @param p_fileName the filename
+	 * @return true if successfully save the map, otherwise return false
+	 * @throws IOException the exception of saving files
+	 */
+	public boolean saveMap(String p_fileName) throws IOException {
 
-			File l_mapFile = new File(l_mapDirectory + p_fileName);
-			
-			d_gameContext.setMapFileName(p_fileName);
-
-			//Specified file name does not exist (new map)
-			if(!l_mapFile.exists() || l_mapFile.isDirectory()) {
-				GenericView.printError("The following map file is invalid, please select another one: " + p_fileName);
-				return;
-			}
-			
-			Scanner l_scanner = new Scanner(l_mapFile);
-			String l_line;
-			String[] l_splitArray;
-			int l_continentCtr = 1;
-			int l_id;
-			Country l_country;
-
-			LoadMapPhase l_loadMapPhase = null;
-			
-			while (l_scanner.hasNextLine()) {
-				l_line = l_scanner.nextLine();
-
-				// determine which part it is
-				// file part
-				if(l_line.equals("[files]")) {
-
-					l_loadMapPhase = LoadMapPhase.FILES;
-					l_line = l_scanner.nextLine();
-				}
-				// continents part
-				else if(l_line.equals("[continents]")) {
-					
-					l_loadMapPhase = LoadMapPhase.CONTINENTS;
-					l_line = l_scanner.nextLine();
-				}
-				//countries part
-				else if (l_line.equals("[countries]")) {
-					
-					l_loadMapPhase = LoadMapPhase.COUNTRIES;
-					l_line = l_scanner.nextLine();
-				}
-				//borders part
-				else if (l_line.equals("[borders]")) {
-					
-					l_loadMapPhase = LoadMapPhase.BORDERS;
-					
-					if(!l_scanner.hasNextLine())
-						l_loadMapPhase = LoadMapPhase.COMPLETE;
-					else{
-						l_line = l_scanner.nextLine();
-					}
-				}
-
-				// read file part
-				if(l_loadMapPhase == LoadMapPhase.FILES) {
-					
-					/*
-					 *  [files]
-					 *	pic europe_pic.jpg
-					 *	map europe_map.gif
-					 *	crd europe.cards
-					 */
-					
-					if(l_line.startsWith("pic")) {
-						
-						d_gameContext.setMapFilePic(l_line.substring(4));
-					}
-					else if(l_line.startsWith("map")) {
-						
-						d_gameContext.setMapFileMap(l_line.substring(4));
-					}
-					else if(l_line.startsWith("crd")) {
-						
-						d_gameContext.setMapFileCards(l_line.substring(4));
-					}
-				}
-				//read continent part
-				else if(l_loadMapPhase == LoadMapPhase.CONTINENTS && !l_line.trim().isEmpty()) {
-					
-					/*
-					 *  [continents]
-					 *	North_Europe 5 red
-					 *	East_Europe 4 magenta
-					 *	South_Europe 5 green
-					 *	West_Europe 3 blue
-					 */
-					
-					l_splitArray = l_line.split("\\s+");
-										
-					d_gameContext.getContinents().put(l_continentCtr,
-							new Continent(l_continentCtr, l_splitArray[0], Integer.parseInt(l_splitArray[1]), l_splitArray[2]));
-					
-					l_continentCtr++;
-				}
-				//read countries part
-				else if(l_loadMapPhase == LoadMapPhase.COUNTRIES && !l_line.trim().isEmpty()) {
-					
-					/*
-					 *  [countries]
-					 *	1 England 1 164 126
-					 *	2 Scotland 1 158 44
-					 *	3 N_Ireland 1 125 70
-					 *	4 Rep_Ireland 1 106 90
-					 */
-					
-					l_splitArray = l_line.split("\\s+");
-					
-					l_id = Integer.parseInt(l_splitArray[0]);
-					l_country = new Country(l_id, l_splitArray[1], Integer.parseInt(l_splitArray[3]),
-							Integer.parseInt(l_splitArray[4]), d_gameContext.getContinents().get(Integer.parseInt(l_splitArray[2])));
-					
-					d_gameContext.getCountries().put(l_id, l_country);
-					
-					d_gameContext.getContinents().get(Integer.parseInt(l_splitArray[2])).getCountries().put(l_id, l_country);
-				}
-				//read border part
-				else if(l_loadMapPhase == LoadMapPhase.BORDERS && !l_line.trim().isEmpty()) {
-					
-					/*
-					 *  [borders]
-					 *	1 8 21 6 7 5 2 3 4
-					 *	2 8 1 3
-					 *	3 1 2
-					 *	4 22 1 5	
-					 */
-					
-					l_splitArray = l_line.split("\\s+");
-					l_country = d_gameContext.getCountries().get(Integer.parseInt(l_splitArray[0]));
-					
-					for(int l_temp = 1; l_temp < l_splitArray.length; l_temp++) {
-						
-						l_id = Integer.parseInt(l_splitArray[l_temp]);
-						l_country.getNeighbors().put(l_id, d_gameContext.getCountries().get(l_id));
-					}
-				}
-			}
-
-			//close reading the file
-			l_scanner.close();
-			
-			GenericView.printSuccess("Map succesfully loaded: " + p_fileName);
-		    
-		} catch (Exception e) {
-			GenericView.printError("An error occured reading the map file: " + p_fileName);
+		// validate if the filename is legal
+		if(p_fileName == null || p_fileName.trim().isEmpty() || p_fileName.trim().length() > 20 ) {
+			GenericView.printError("InValid File Name, please type a valid file name, with length less than 20.");
+			return false;
 		}
-	 }		
-	 public void validateMap() {
-		 //todo
-	 }	
 
-	 public void addPlayer(String p_playerName) {
+		if(! d_mapService.validateMap(d_gameContext) ) {
+			GenericView.printError("InValid map, please check the map.");
+			return false;
+		}
+
+		// call mapService to save the map and return the path
+		p_fileName = p_fileName.trim();
+		try{
+			if(d_mapService.saveMap(p_fileName)) {
+				GenericView.printSuccess("Map was saved in :" + this.d_gameContext.getMapfolder() + p_fileName );
+				return true;
+			}
+			else {
+				GenericView.printError("Exception occured when saving the map, please valid the file name or contact the Administrator.");
+				return false;
+			}
+		}
+		catch(Exception ex) {
+			GenericView.printError("Exception occured when saving the map. " + ex.toString());
+			return false;
+		}
+	}
+
+	/**
+	 * Performs the action for the user command: editmap filename
+	 *
+	 * Load a map from an existing "domination" map file,
+	 * or create a new map from scratch if the file does not exist
+	 * @param p_fileName the filename
+	 * @return true if successfully edit the map, otherwise return false
+	 */
+	public boolean editMap (String p_fileName) {
+		return d_mapService.editMap(p_fileName);
+	}
+
+	/**
+	 * Performs the action for the user command: validatemap
+	 *
+	 * Verification of map correctness. The map should be automatically validated upon loading
+	 * and before saving (at least 3 types of incorrect maps). The validatemap command can be
+	 * triggered any time during map editing.
+	 * @return true if it is a valid map, otherwise return false
+	 */
+	public boolean validateMap() {
+		if(! d_mapService.validateMap(d_gameContext) ) {
+			GenericView.printError("It is not a connected map.");
+			return false;
+		}
+		else {
+			GenericView.printSuccess("Yeah! You got a connected map!");
+			return true;
+		}
+	}
+
+	public void addPlayer(String p_playerName) {
 		 printInvalidCommandMessage();
 	 }	
 	 public void removePlayer(String p_playerName){
