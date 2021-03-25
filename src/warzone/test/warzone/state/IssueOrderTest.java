64efdacdabf0e8bf98d827ee0d3f987a70db12c9
elliansjm <ellian98765@gmail.com>
2021-03-24 22:31:44 -0400

    add phase test cases,rename TestSuite

--- src/warzone/test/warzone/state/IssueOrderTest.java
+++ src/warzone/test/warzone/state/IssueOrderTest.java
@@ -1,10 +1,24 @@
 package warzone.state;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
-/**
- * test class of issue order
+import warzone.model.GameContext;
+import warzone.model.GamePhase;
+import warzone.service.GameEngine;
+
+/** 
+ * Test class for issue order phase
  */
 public class IssueOrderTest {
-
+	
+	@Test
+	public void inputNextCommand() {
+		GameContext l_gameContext = GameContext.getGameContext();
+		GameEngine l_gameEngine = GameEngine.getGameEngine(l_gameContext);
+		IssueOrder l_issueOrderState = new IssueOrder(l_gameEngine);
+		l_gameEngine.setPhase(l_issueOrderState);
+		l_issueOrderState.next();
+		assert(l_gameEngine.getPhase().getGamePhase()==GamePhase.OrderExecution);
+	}
+	
 }
--- src/warzone/test/warzone/state/ReinforcementTest.java
+++ src/warzone/test/warzone/state/ReinforcementTest.java
@@ -1,10 +1,22 @@
 package warzone.state;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
-/**
- * test class of reinforcement
+import warzone.model.GameContext;
+import warzone.model.GamePhase;
+import warzone.service.GameEngine;
+
+/** 
+ * Test class for reinforcement phase
  */
 public class ReinforcementTest {
-
+	@Test
+	public void inputNextCommand() {
+		GameContext l_gameContext = GameContext.getGameContext();
+		GameEngine l_gameEngine = GameEngine.getGameEngine(l_gameContext);
+		Reinforcement l_reinforcementState = new Reinforcement(l_gameEngine);
+		l_gameEngine.setPhase(l_reinforcementState);
+		l_reinforcementState.next();
+		assert(l_gameEngine.getPhase().getGamePhase()==GamePhase.IssueOrder);
+	}
 }
