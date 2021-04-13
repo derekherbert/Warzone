d62c646dd645e1a6425858c7239af42e048d3223
LinusPeng97 <linuspeng97@outlook.com>
2021-04-13 21:38:18 +0800

    add new test files in view

--- src/warzone/test/warzone/view/ViewTestSuite.java
+++ src/warzone/test/warzone/view/ViewTestSuite.java
@@ -8,7 +8,8 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	GenericViewTest.class
+	GenericViewTest.class,
+	TournamentResultsView.class,
 	})
 public class ViewTestSuite {
 
